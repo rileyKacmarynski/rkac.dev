72244511b46c7cd9c4a87859ffb20c11a176ad74
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-06-03 15:20:05 -0500

    set up rehype I hope

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,5 +1,10 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 import readingTime from 'reading-time'
+import rehypeSlug from 'rehype-slug'
+import rehypAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypePrettyCode from 'rehype-pretty-code'
+import remarkGfm from 'remark-gfm'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -27,4 +32,40 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: './content',
   documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCodeTitles,
+      [
+        rehypePrettyCode,
+        {
+          theme: 'one-dark-pro',
+          onVisitLine(node: any) {
+            // Prevent lines from collapsing in `display: grid` mode, and
+            // allow empty lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }]
+            }
+          },
+          onVisitHighlightedLine(node: any) {
+            // Each line node by default has `class="line"`.
+            node.properties.className.push('highlighted')
+          },
+          onVisitHighlightedWord(node: any) {
+            // Each word node has no className by default.
+            node.properties.className = ['word']
+          },
+        },
+      ],
+      [
+        rehypAutolinkHeadings,
+        {
+          properties: {
+            className: ['heading'],
+          },
+        },
+      ],
+    ],
+  },
 })
