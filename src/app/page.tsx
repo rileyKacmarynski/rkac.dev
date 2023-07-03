088875825a18e78503a8bec1723f937092e38061
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-07-03 12:07:35 -0500

    add cheesy header for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import { compareDesc, format, parseISO } from 'date-fns'
-import { allPosts, Post } from 'contentlayer/generated'
+import { allPosts } from 'contentlayer/generated'
 import Balancer from 'react-wrap-balancer'
 import Link from 'next/link'
 import { MoveRight } from 'lucide-react'
-import { Button } from '@/components/ui/button'
 import ThemeToggle from '@/components/theme-toggle'
 
 export default function Home() {
@@ -23,7 +22,7 @@ export default function Home() {
         >
           skip to content
         </a>
-        <nav className="flex items-center h-full max-w-6xl m-auto">
+        <nav className="flex items-center h-full max-w-3xl m-auto">
           <Link href="/" className="block">
             <span className="inline-block text-3xl font-semibold tracking-tight -skew-x-6">
               rkac
@@ -34,42 +33,62 @@ export default function Home() {
         </nav>
       </header>
       <div id="skip-nav" className="scroll-m-16" tabIndex={-1} />
-      <main className="max-w-6xl px-4 mx-auto mt-6">
-        <div className="h-[420px] text-center grid items-center">
-          <p className="">We&apos;ll put a some header content here</p>
-        </div>
-        <h2 className="mb-12 text-3xl font-bold tracking-tight text-center text-transparent bg-clip-text dark:from-white dark:to-neutral-400 from-black to-neutral-700 bg-gradient-to-b">
-          Recent Posts
-        </h2>
-        <div className="flex flex-col w-full gap-4 sm:grid sm:grid-cols-2 md:grid-cols-3">
-          {posts.map((post) => (
-            <article key={post._id} className="basis-full">
-              <Link
-                href={post.slug}
-                className="flex flex-col sm:h-full h-[240px] p-6 no-underline transition border shadow group rounded-xl border-border dark:hover:bg-white/[2.5%] dark:hover:border-zinc-700 hover:bg-black/[5%] hover:border-zinc-300 hover:cursor-pointer"
-              >
-                <header className="mb-4">
-                  <h2 className="m-0 text-2xl font-bold tracking-tight text-transparent text-gray-900 bg-clip-text dark:from-white dark:to-neutral-200 from-black to-neutral-800 bg-gradient-to-b ">
-                    <Balancer>{post.title}</Balancer>
-                  </h2>
-                  <p className="mt-1 space-x-1 text-xs text-muted-foreground">
-                    <span>{format(parseISO(post.date), 'MMMM dd, yyyy')}</span>
-                    <span>{` • `}</span>
-                    <span>{post.readingTime.text}</span>
-                  </p>
-                </header>
-                {post.description && (
-                  <p className="mt-auto mb-6 font-normal max-h-32 line-clamp-2 sm:line-clamp-4">
-                    {post.description}
-                  </p>
-                )}
-                <footer className="flex items-end gap-3 transition text-muted-foreground group-hover:text-foreground">
-                  <span className="ml-auto">Read more</span>
-                  <MoveRight className="transition group-hover:translate-x-1" />
-                </footer>
-              </Link>
-            </article>
-          ))}
+      <main className="px-6 mt-6">
+        <div className="max-w-3xl mx-auto">
+          <div className="h-[420px] ">
+            <section className="">
+              <h1 className="text-2xl font-bold tracking-tight mt-14">
+                Hey! I'm Riley 👋
+              </h1>
+              <div className="prose prose-zinc dark:prose-invert">
+                <p className="mt-4">
+                  {` I'm a software developer, from South Dakota. While I've filled the
+              role of a fullstack developer in most of my day-to-day career, the
+              last few years I've been focusing on front-end development with
+              React and Typescript.`}
+                </p>
+                <p className="mt-2">
+                  I love building things on the web and hope to share what I
+                  learn here.
+                </p>
+              </div>
+            </section>
+          </div>
+          <h2 className="mb-12 text-3xl font-bold tracking-tight text-center text-transparent bg-clip-text dark:from-white dark:to-neutral-400 from-black to-neutral-700 bg-gradient-to-b">
+            Recent Posts
+          </h2>
+          <div className="flex flex-col w-full gap-4 sm:grid sm:grid-cols-2 ">
+            {posts.map((post) => (
+              <article key={post._id} className="basis-full">
+                <Link
+                  href={post.slug}
+                  className="flex flex-col sm:h-full h-[240px] p-6 no-underline transition border shadow group rounded-xl border-border dark:hover:bg-white/[2.5%] dark:hover:border-zinc-700 hover:bg-black/[5%] hover:border-zinc-300 hover:cursor-pointer"
+                >
+                  <header className="mb-4">
+                    <h2 className="m-0 text-2xl font-bold tracking-tight text-transparent text-gray-900 bg-clip-text dark:from-white dark:to-neutral-200 from-black to-neutral-800 bg-gradient-to-b ">
+                      <Balancer>{post.title}</Balancer>
+                    </h2>
+                    <p className="mt-1 space-x-1 text-xs text-muted-foreground">
+                      <span>
+                        {format(parseISO(post.date), 'MMMM dd, yyyy')}
+                      </span>
+                      <span>{` • `}</span>
+                      <span>{post.readingTime.text}</span>
+                    </p>
+                  </header>
+                  {post.description && (
+                    <p className="mt-auto mb-6 font-normal max-h-32 line-clamp-2 sm:line-clamp-4">
+                      {post.description}
+                    </p>
+                  )}
+                  <footer className="flex items-center gap-3 transition text-muted-foreground group-hover:text-foreground">
+                    <span className="ml-auto">Read more</span>
+                    <MoveRight className="mt-[4px] transition group-hover:translate-x-1" />
+                  </footer>
+                </Link>
+              </article>
+            ))}
+          </div>
         </div>
       </main>
     </>
