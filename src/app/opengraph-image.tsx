5c6e79708a6fdb47d06188d91a089daf47858111
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-07-11 15:57:12 -0500

    trying to figure out OG image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,56 @@
+import { ImageResponse } from 'next/server'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'rkac.dev'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+// https://github.com/vercel/next.js/issues/48081
+// Font has to be inside route handler I guess?
+
+// Image generation
+export default async function Image() {
+  const interSemiBold = fetch(
+    new URL('/public/Inter-Regular.ttf', import.meta.url)
+  ).then((res) => res.arrayBuffer())
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        rkac.dev
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: 'Inter',
+          data: await interSemiBold,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  )
+}
