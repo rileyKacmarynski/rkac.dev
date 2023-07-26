b8681e4e69984cc7da6e71992c3d4f1d77f6d158
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-07-26 18:19:31 -0500

    ok were leaving the og image how it is now...moving on

--- src/app/blog/[slug]/opengraph-image.tsx
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -1,7 +1,12 @@
-import { getPostFromParams, mapTagsToIcon } from './helpers'
+import { Post } from 'contentlayer/generated'
+import { getPostFromParams } from './helpers'
 import { PostProps } from './page'
 import OGImage from '@/components/og-image'
 import { ImageResponse } from 'next/server'
+import NextJSIcon from '@/components/icons/nextjs-icon'
+import ReactIcon from '@/components/icons/react-icon'
+import JavascriptIcon from '@/components/icons/javascript-icon'
+import TypescriptIcon from '@/components/icons/typescript-icon'
 
 // Route segment config
 export const runtime = 'edge'
@@ -25,26 +30,62 @@ export default async function Image({ params }: PostProps) {
   const post = await getPostFromParams(params)
   const Icon = mapTagsToIcon(post?.tags ?? [])
 
-  return new ImageResponse(
-    (
-      <OGImage>
-        {Icon ? (
-          <Icon style={{ color: 'hsl(0, 100%, 100%, .5)' }} />
-        ) : (
-          'rkac.dev'
-        )}
-      </OGImage>
-    ),
-    {
-      ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await inter,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
+  return new ImageResponse(<OGImage>{Icon ? Icon : 'rkac.dev'}</OGImage>, {
+    ...size,
+    fonts: [
+      {
+        name: 'Inter',
+        data: await inter,
+        style: 'normal',
+        weight: 400,
+      },
+    ],
+  })
+}
+
+function mapTagsToIcon(tags: Post['tags']) {
+  if (!tags || !tags.length) {
+    return null
+  }
+
+  // not a huge fan of this
+  // already making typos between here and the frontmatter
+  for (const tag of tags) {
+    switch (tag) {
+      case 'Next.js':
+        return <NextJSIcon />
+      case 'React':
+        return (
+          <ReactIcon
+            style={{
+              height: '320px',
+              width: '320px',
+              color: 'hsl(0, 100%, 100%, .25)',
+            }}
+          />
+        )
+      case 'JavaScript':
+        return (
+          <JavascriptIcon
+            style={{
+              height: '320px',
+              width: '320px',
+              color: 'hsl(0, 100%, 100%, .25)',
+            }}
+          />
+        )
+      case 'TypeScript':
+        return (
+          <TypescriptIcon
+            style={{
+              height: '320px',
+              width: '320px',
+              color: 'hsl(0, 100%, 100%, .25)',
+            }}
+          />
+        )
+      default:
+        return null
     }
-  )
+  }
 }
