6b23c935e6e164d075ce66112527fbd9809ded77
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-08-09 13:21:44 -0500

    shits not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { compareDesc, format, parseISO } from 'date-fns'
-import { allPosts } from 'contentlayer/generated'
+import { Post, allPosts } from 'contentlayer/generated'
 import Balancer from 'react-wrap-balancer'
 import Link from 'next/link'
 import { MoveRight } from 'lucide-react'
@@ -7,6 +7,7 @@ import { buttonVariants } from '@/components/ui/button-variants'
 import { cn } from '@/lib/utils'
 import GitHubIcon from '@/components/icons/github-icon'
 import Header from '@/components/header'
+// import PostCard from '@/components/post-card'
 
 export default function Home() {
   let posts = allPosts.sort((a, b) =>
@@ -52,38 +53,41 @@ export default function Home() {
           </h2>
           <div className="flex flex-col w-full gap-4 sm:grid sm:grid-cols-2 ">
             {posts.map((post) => (
-              <article key={post._id} className="basis-full">
-                <Link
-                  href={post.slug}
-                  className="flex flex-col sm:h-full h-[240px] p-6 no-underline transition border shadow group rounded-xl border-border dark:hover:bg-white/[2.5%] dark:hover:border-zinc-700 hover:bg-black/[5%] hover:border-zinc-300 hover:cursor-pointer"
-                >
-                  <header className="mb-4">
-                    <h2 className="m-0 text-2xl font-bold tracking-tight text-transparent text-gray-900 bg-clip-text dark:from-white dark:to-neutral-200 from-black to-neutral-800 bg-gradient-to-b ">
-                      <Balancer>{post.title}</Balancer>
-                    </h2>
-                    <p className="mt-1 space-x-1 text-xs text-muted-foreground">
-                      <span>
-                        {format(parseISO(post.date), 'MMMM dd, yyyy')}
-                      </span>
-                      <span>{` • `}</span>
-                      <span>{post.readingTime.text}</span>
-                    </p>
-                  </header>
-                  {post.description && (
-                    <p className="mt-auto mb-6 font-normal max-h-32 line-clamp-2 sm:line-clamp-4">
-                      {post.description}
-                    </p>
-                  )}
-                  <footer className="flex items-center gap-3 transition text-muted-foreground group-hover:text-foreground">
-                    <span className="ml-auto">Read more</span>
-                    <MoveRight className="mt-[4px] transition group-hover:translate-x-1" />
-                  </footer>
-                </Link>
-              </article>
+              <PostCard key={post._id} post={post} />
             ))}
           </div>
         </div>
       </div>
     </>
+  )
+}
+
+export function PostCard({ post }: { post: Post }) {
+  // <motion.div className="absolute transition duration-300 bg-indigo-800 opacity-0 pointer-events-none -inset-px rounded-xl group:hover:opacity-100" />
+  return (
+    <Link
+      href={post.slug}
+      className="flex flex-col sm:h-full h-[240px] p-6 no-underline transition border shadow group rounded-xl border-border dark:hover:bg-white/[2.5%] dark:hover:border-zinc-700 hover:bg-black/[5%] hover:border-zinc-300 hover:cursor-pointer"
+    >
+      <header className="mb-4">
+        <h2 className="m-0 text-2xl font-bold tracking-tight text-transparent text-gray-900 bg-clip-text dark:from-white dark:to-neutral-200 from-black to-neutral-800 bg-gradient-to-b ">
+          <Balancer>{post.title}</Balancer>
+        </h2>
+        <p className="mt-1 space-x-1 text-xs text-muted-foreground">
+          <span>{format(parseISO(post.date), 'MMMM dd, yyyy')}</span>
+          <span>{` • `}</span>
+          <span>{post.readingTime.text}</span>
+        </p>
+      </header>
+      {post.description && (
+        <p className="mt-auto mb-6 font-normal max-h-32 line-clamp-2 sm:line-clamp-4">
+          {post.description}
+        </p>
+      )}
+      <footer className="flex items-center gap-3 transition text-muted-foreground group-hover:text-foreground">
+        <span className="ml-auto">Read more</span>
+        <MoveRight className="mt-[4px] transition group-hover:translate-x-1" />
+      </footer>
+    </Link>
   )
 }
