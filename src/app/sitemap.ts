7501a31e7ee739db9d1def53c665209dc33ef6a1
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2024-06-02 08:40:46 -0500

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,10 @@
+import { getBlogPosts } from '@/app/blog/utils'
 import { formatISO } from 'date-fns'
 
 export default async function sitemap() {
-  const posts = allPosts.map((post) => ({
+  const posts = getBlogPosts().map((post) => ({
     url: `https://rkac.dev/blog/${post.slug}`,
-    lastModified: post.date,
+    lastModified: post.data.date,
   }))
 
   const routes = ['', '/blog'].map((route) => ({
